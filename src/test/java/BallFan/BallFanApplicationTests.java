ba79251ae27b5e2307fa0e8975c4dc5577e2b015
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-03-24 01:55:35 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/test/java/BallFan/BallFanApplicationTests.java
@@ -0,0 +1,13 @@
+package BallFan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BallFanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
