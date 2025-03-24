5b96fd5f32cb682dad5607d3f8d4f78621fcfe2b
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-03-24 18:34:30 +0900

    feat: LineUp Entity 생성

---
+++ src/main/java/BallFan/entity/LineUp.java
@@ -0,0 +1,37 @@
+package BallFan.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "LineUp")
+public class LineUp {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "line_up_id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "team")
+    private Team team;
+
+    @Column(name = "order")
+    private int order;
+
+    @Column(name = "position")
+    private String position;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "game_result_id")
+    private GameResult gameResult;
+}
