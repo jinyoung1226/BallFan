cdca79c79279c4985b522844665668c2dcc4a9f0
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-03-25 03:01:02 +0900

    feat: JWT, Security 관련 필터 및 provider 생성

---
+++ src/main/java/BallFan/authentication/UserDetailsServiceImpl.java
@@ -0,0 +1,48 @@
+package BallFan.authentication;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import BallFan.entity.User;
+import BallFan.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private static final String USER_NOT_FOUND_EXCEPTION = "존재하지 않는 회원입니다.";
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findUserByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException(USER_NOT_FOUND_EXCEPTION));
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), getAuthorities(user));
+    }
+
+    private Collection<? extends GrantedAuthority> getAuthorities(Object user) {
+        // 권한 정보를 GrantedAuthority 객체 컬렉션으로 반환
+        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    public User getUserByContextHolder() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        UserDetails userDetails = (UserDetails) principal;
+        return userRepository.findUserByEmail(userDetails.getUsername())
+                .orElseThrow(() -> new UsernameNotFoundException(USER_NOT_FOUND_EXCEPTION));
+    }
+
+    public Authentication getAuthentication(){
+        return SecurityContextHolder.getContext().getAuthentication();
+    }
+}
+
