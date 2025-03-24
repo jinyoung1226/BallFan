31b67713ee742df16bf5109ad89bf39e99d806ad
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-03-24 18:32:22 +0900

    feat: ReviewRepository 생성

---
+++ src/main/java/BallFan/repository/ReviewRepository.java
@@ -0,0 +1,9 @@
+package BallFan.repository;
+
+import BallFan.entity.review.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+}
