a44a836bc7786c88419819410c34464df516a0c8
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-03-24 18:34:24 +0900

    feat: PitcherStat Entity 생성

---
+++ src/main/java/BallFan/entity/pitcher/PitcherStat.java
@@ -0,0 +1,36 @@
+package BallFan.entity.pitcher;
+
+import BallFan.entity.GameResult;
+import BallFan.entity.Team;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "PitcherStat")
+public class PitcherStat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "pitcher_stat_id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "team")
+    private Team team;
+
+    @Column(name = "pitcher_type")
+    private PitcherType pitcherType;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "game_result_id")
+    private GameResult gameResult;
+}
