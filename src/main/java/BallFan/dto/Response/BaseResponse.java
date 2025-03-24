5858fd2f183902785812bc4d333447bcdde2c4e2
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-03-25 02:55:41 +0900

    feat: BaseResponse 기본 응답 객체 생성

---
+++ src/main/java/BallFan/dto/Response/BaseResponse.java
@@ -0,0 +1,13 @@
+package BallFan.dto.Response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@SuperBuilder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class BaseResponse {
+    protected String message;
+    private Object data;
+}
