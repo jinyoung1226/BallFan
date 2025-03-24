a025b8309a92ba02797fa1bac8bbe71a3a25d921
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-03-24 18:34:18 +0900

    feat: Review Entity 생성

---
+++ src/main/java/BallFan/entity/review/Review.java
@@ -0,0 +1,46 @@
+package BallFan.entity.review;
+
+import BallFan.entity.Ticket;
+import BallFan.entity.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "Review")
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "review_id")
+    private Long id;
+
+    @Column(name = "seat")
+    private String seat;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "likes")
+    private int likes;
+
+    @Column(name = "created_at")
+    private LocalDate createdAt;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ticket_id")
+    private Ticket ticket;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}
