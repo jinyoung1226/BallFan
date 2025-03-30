539e633a03b6b8f39b633760425f26f8535a2f96
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-03-30 20:15:52 +0900

    feat: UserDTO 생성

---
+++ src/main/java/BallFan/dto/user/UserDTO.java
@@ -0,0 +1,33 @@
+package BallFan.dto.user;
+
+import BallFan.entity.Team;
+import BallFan.entity.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+
+    private Long id;
+    private String nickname;
+    private Team team;
+    private String image;
+    private int currentWinStreak;
+    private int monthlyWinCount;
+
+    public static UserDTO from(User user) {
+        return UserDTO.builder()
+                .id(user.getId())
+                .nickname(user.getNickname())
+                .team(user.getTeam())
+                .currentWinStreak(user.getCurrentWinStreak())
+                .monthlyWinCount(user.getMonthlyWinCount())
+                .build();
+    }
+
+}
