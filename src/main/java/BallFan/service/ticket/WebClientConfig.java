692cf26bb74880e4b46a9d316bfe18156746a90b
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-04-01 04:05:19 +0900

    feat: WebClientConfig 생성

--- src/main/java/BallFan/service/ticket/WebClientConfig.java
+++ src/main/java/BallFan/service/ticket/WebClientConfig.java
@@ -10,7 +10,7 @@ public class WebClientConfig {
     @Bean
     public WebClient webClient() {
         return WebClient.builder()
-                .baseUrl("http://ticket-ocr:8000") // 보내는 곳의 URL(필자는 도커 컨테이너로 같은 네트워크로 하기 때문에, 컨테이너 호스트명을 넣으면 됨)
+                .baseUrl("http://ballfan-ocr:8000") // 보내는 곳의 URL(필자는 도커 컨테이너로 같은 네트워크로 하기 때문에, 컨테이너 호스트명을 넣으면 됨)
                 .defaultHeader("Content-Type", "application/json") // 공통 헤더
                 .build();
     }
