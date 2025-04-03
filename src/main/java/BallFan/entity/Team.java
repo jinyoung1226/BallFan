1fd2d007b6872c888570a90e0810d4692ab441aa
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-04-03 22:22:56 +0900

    feat: Enum 상수 String 할당 -> toString으로 Overriding

--- src/main/java/BallFan/entity/Team.java
+++ src/main/java/BallFan/entity/Team.java
@@ -1,5 +1,27 @@
 package BallFan.entity;
 
 public enum Team {
-    삼성, LG, SSG, KIA, KT, 한화, NC, 두산, 롯데, 키움
+    삼성("삼성 라이온즈"),
+    LG("LG 트윈스"),
+    SSG("SSG 랜더스"),
+    KIA("KIA 타이거즈"),
+    KT("KT wiz"),
+    한화("한화 이글스"),
+    NC("NC 다이노스"),
+    두산("두산 베어스"),
+    롯데("롯데 자이언츠"),
+    키움("키움 히어로즈");
+
+    private final String fullName;
+
+    Team(String fullName) {
+        this.fullName = fullName;
+    }
+
+    @Override
+    public String toString() {
+        return fullName;
+    }
+
+
 }
