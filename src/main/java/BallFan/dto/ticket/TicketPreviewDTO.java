fda63b0168dc200a902d2d509d9ed10101ec8e46
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-04-03 22:23:09 +0900

    feat: TicketPreviewDTO 생성

---
+++ src/main/java/BallFan/dto/ticket/TicketPreviewDTO.java
@@ -0,0 +1,24 @@
+package BallFan.dto.ticket;
+
+import BallFan.entity.Team;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TicketPreviewDTO {
+
+    private Long id;
+    private String stadium;
+    private LocalDate ticketDate;
+    private String dayOfWeek;
+    private Team homeTeam;
+    private Team awayTeam;
+
+}
