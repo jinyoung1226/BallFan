80edd23f0976aafcfd25f396b1a80eed49cc1ab3
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-04-03 22:23:30 +0900

    fix: 오타 수정

--- src/main/java/BallFan/dto/auth/SignInRequest.java
+++ src/main/java/BallFan/dto/auth/SignInRequest.java
@@ -4,9 +4,11 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class SignInRequest {
     @NotBlank(message = "이메일은 필수 입력 항목입니다.")
