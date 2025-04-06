a162f4c0c81fc8ec143b1d600a8fb6caf0a06495
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-04-06 22:06:28 +0900

    feat: TicketDetailNotFoundException 생성

--- src/main/java/BallFan/exception/GlobalExceptionHandler.java
+++ src/main/java/BallFan/exception/GlobalExceptionHandler.java
@@ -1,10 +1,11 @@
 package BallFan.exception;
 
-import BallFan.dto.Response.ErrorResponse;
+import BallFan.dto.response.ErrorResponse;
 import BallFan.exception.auth.BlackListedTokenException;
 import BallFan.exception.auth.DifferentRefreshTokenException;
 import BallFan.exception.auth.DuplicatedSignUpException;
 import BallFan.exception.ticket.DuplicatedTicketException;
+import BallFan.exception.ticket.TicketDetailNotFoundException;
 import BallFan.exception.ticket.TicketNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -50,4 +51,12 @@ public class GlobalExceptionHandler {
                 .message(e.getMessage())
                 .build(), HttpStatus.NOT_FOUND);
     }
+
+    @ExceptionHandler(TicketDetailNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleTicketDetailNotFoundException(TicketDetailNotFoundException e) {
+        return new ResponseEntity<>(ErrorResponse.builder()
+                .message(e.getMessage())
+                .build(), HttpStatus.NOT_FOUND);
+    }
+
 }
