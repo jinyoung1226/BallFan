105f6accec3b0fe6959203db48e8cbc45411e2d7
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-04-06 20:51:09 +0900

    fix: 예외 삭제

--- src/main/java/BallFan/exception/ticket/FavoriteTeamTicketNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package BallFan.exception.ticket;
-
-public class FavoriteTeamTicketNotFoundException extends RuntimeException {
-    public FavoriteTeamTicketNotFoundException(String message) {
-        super(message);
-    }
-}
