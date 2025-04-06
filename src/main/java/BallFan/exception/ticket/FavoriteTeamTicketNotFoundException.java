184da5af0043bd26f2739464cb5bf6b4412712fa
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-04-06 18:40:46 +0900

    feat: 티켓 예외 추가

---
+++ src/main/java/BallFan/exception/ticket/FavoriteTeamTicketNotFoundException.java
@@ -0,0 +1,7 @@
+package BallFan.exception.ticket;
+
+public class FavoriteTeamTicketNotFoundException extends RuntimeException {
+    public FavoriteTeamTicketNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/BallFan/exception/ticket/TicketDetailNotFoundException.java
@@ -0,0 +1,7 @@
+package BallFan.exception.ticket;
+
+public class TicketDetailNotFoundException extends RuntimeException {
+    public TicketDetailNotFoundException(String message) {
+        super(message);
+    }
+}
