5b08d52750aa5801c5d7478ac4c327be6dbff6b5
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-05-19 23:20:03 +0900

    fix: 사용하지 않는 repository 삭제

--- src/main/java/BallFan/repository/LineUpRepository.java
+++
@@ -1,9 +0,0 @@
-package BallFan.repository;
-
-import BallFan.entity.LineUp;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LineUpRepository extends JpaRepository<LineUp, Long> {
-}
--- src/main/java/BallFan/repository/PitcherStatRepository.java
+++
@@ -1,9 +0,0 @@
-package BallFan.repository;
-
-import BallFan.entity.pitcher.PitcherStat;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PitcherStatRepository extends JpaRepository<PitcherStat, Long> {
-}
