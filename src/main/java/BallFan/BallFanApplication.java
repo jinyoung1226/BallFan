b2b72dc3428389eefbe5c641ec2d6df8641fbf8d
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-06-07 23:33:00 +0900

    fix : 로그인 이메일 및 비밀번호 틀릴 때, 분기 처리

--- src/main/java/BallFan/BallFanApplication.java
+++ src/main/java/BallFan/BallFanApplication.java
@@ -2,10 +2,8 @@ package BallFan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "BallFan.repository") // JPA용
 public class BallFanApplication {
 
 	public static void main(String[] args) {
