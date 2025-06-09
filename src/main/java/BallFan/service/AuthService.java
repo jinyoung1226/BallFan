865724eb7d56973e83bc2ba8743ff845cbaaec26
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2025-06-10 01:20:13 +0900

    fix : 디버깅

--- src/main/java/BallFan/service/AuthService.java
+++ src/main/java/BallFan/service/AuthService.java
@@ -47,7 +47,7 @@ public class AuthService {
                     .email(request.getEmail())
                     .password(passwordEncoder.encode(request.getPassword()))
                     .nickname(request.getNickname())
-                    .image("https://b-cube-web.s3.ap-northeast-2.amazonaws.com/b-cube-web/default_profile.svg")
+                    .image("https://b-cube-web.s3.ap-northeast-2.amazonaws.com/b-cube-web/%E1%84%89%E1%85%B3%E1%84%8F%E1%85%B3%E1%84%85%E1%85%B5%E1%86%AB%E1%84%89%E1%85%A3%E1%86%BA+2025-06-10+%E1%84%8B%E1%85%A9%E1%84%8C%E1%85%A5%E1%86%AB+1.19.01.png")
                     .team(request.getTeam())
                     .build();
             userRepository.save(user);
